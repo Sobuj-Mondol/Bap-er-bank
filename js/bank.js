014d47e784f57df659afe890b14328414d99d669
Sobuj <sobujkumar424@gmail.com>
2022-02-10 19:26:24 +0600

    added js-bank.js

---
+++ js/bank.js
@@ -0,0 +1,13 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    let emailFild = document.getElementById('user-email');
+    let userEmail = emailFild.value;
+    // get user password
+    let passwordFild = document.getElementById('user-password');
+    let userPassword = passwordFild.value;
+    if(userEmail == 'sobuj@gmail.com' && userPassword == 12345){
+        window.location.href = 'Banking-site';
+    }
+    else{
+        console.log('srroy sir your information not valid')
+    }
+})
